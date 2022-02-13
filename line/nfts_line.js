711c0b5cedbc2d0910da275c31e81a47617964a4
Penelope Richmond <peneloperichmond@hotmail.co.uk>
2022-02-13 19:08:48 +0000

    x and y

--- line/nfts_line.js
+++ line/nfts_line.js
@@ -3,4 +3,8 @@ async function drawLineChart() {
     // 1. Access data
     const dataset = await d3.json("./data/nfts.csv")
     console.log(dataset[0])
+
+    const yAccessor = d => d.stats.market_cap
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    const xAccessor = d => dateParser(d.created_date)
 }
